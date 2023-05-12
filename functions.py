1b3480e3772796a1595390fd5574cafba0065d4f
Elly Poretsky <elly.poretsky@gmail.com>
2023-05-12 14:22:55 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import gzip
 import glob
 import os
+import re
 
 from collections import OrderedDict
 from Bio import SeqIO
@@ -14,7 +15,7 @@ import h5py
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def load_fasta(fasta_path):
+def fasta_to_dict(fasta_path):
     """
     Load a fasta file and parse it using biopython
     
@@ -28,11 +29,28 @@ def load_fasta(fasta_path):
     """
     
     seq_dict = OrderedDict()
-
+    #print(fasta_path)
     record_iterator = SeqIO.parse(fasta_path, "fasta")
     for record in record_iterator:
-        seq_dict[str(record.id)] = str(record.seq)
+        seq_dict[str(record.id)] = record
+        #seq_dict[str(record.id)] = str(record.seq)
+    return(seq_dict)
+
+def fasta_to_dataframe(fasta_path):
+    """
+    Load a fasta file and parse it using biopython
     
+    - Input
+    fasta_path : str
+        relative path of the fasta file
+
+    - Output
+    sec_dict : pandas dataframe
+        dictionary with gene IDs as keys and sequences as string values
+    """
+    
+    seq_dict = fasta_to_dict(fasta_path)
+
     # Convert fasta dict to PDF for a simpler merge  
     seq_df  = pd.Series(seq_dict, index=seq_dict.keys())
     seq_df = pd.DataFrame(seq_df)
@@ -40,6 +58,26 @@ def load_fasta(fasta_path):
     seq_df.columns = ["gene_id", "seq"]
     return(seq_df)
 
+
+def extract_fasta_sites(fasta_path, sites):
+    record_iterator = SeqIO.parse(fasta_path, "fasta")
+    site_list = []
+    for record in record_iterator:
+        for pattern in sites:
+            indices = re.finditer(pattern=pattern, string=str(record.seq))
+            for ix in indices:
+                ix = ix.start()
+                site_list.append(str(record.id)+"_"+pattern+str(ix+1))
+    return(site_list)
+
+def make_phospholingo_prediction_fasta(fasta_path, sites, output_path):
+    seq_dict = fasta_to_dict(fasta_path)
+    for key, val in seq_dict.items():
+        for site in sites:
+            seq_dict[key].seq = val.seq.replace(site, site+"#")
+    
+    with open(output_path, 'w') as handle:
+        SeqIO.write(seq_dict.values(), handle, 'fasta')
 
 def sites_table(input_path, input_type, output_path=None):
     """
